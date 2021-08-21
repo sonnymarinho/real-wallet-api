65bb390c9721538064500d67a8f076285ffc7a8c
Sonny <sonny.marinho@gmail.com>
2021-08-21 14:55:45 -0300

    :wrench: chore: prisma configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from '@resources/users/users.module';
 import { AuthModule } from '@root/resources/auth/auth.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    AuthModule,
-    MongooseModule.forRoot('mongodb://localhost/real-wallet:dev'),
-  ],
+  imports: [UsersModule, AuthModule],
 })
 export class AppModule {}
