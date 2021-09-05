2aaa5ec726e55c9756faf278a42e09dd1a983e51
Sonny <sonny.marinho@gmail.com>
2021-09-05 16:12:36 -0300

    :test_tube: users repository tests

--- src/resources/users/users.controller.spec.ts
+++ src/resources/users/users.controller.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   const mockUserService = {
-    create: jest.fn((dto) => ({
-      id: new Date().toString(),
+    create: jest.fn((dto: CreateUserDto) => ({
+      id: uuidv4(),
       ...dto,
     })),
 
@@ -36,20 +38,21 @@ describe('UsersController', () => {
   });
 
   describe('create', () => {
-    it('should be ablel to create a new user', async () => {
+    it('should be able to create a new user', async () => {
       const data = { name: 'Jhon', password: '123', email: 'name@domain.com' };
 
-      expect(controller.create(data)).toEqual({
-        id: expect.any(String),
-        ...data,
-      });
+      const user = await controller.create(data);
 
       expect(mockUserService.create).toBeCalledWith(data);
+      expect(user).toEqual({
+        id: expect.any(String),
+        ...user,
+      });
     });
   });
 
   describe('update', () => {
-    it('should be ablel to update a user', async () => {
+    it('should be able to update a user', async () => {
       const data = { name: 'Jhon', password: '123' };
 
       expect(controller.update('123', data)).toEqual({
